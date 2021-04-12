7cfd547505a9d019595d862a3ff91e9b12ab863c
atgiphy3750 <plastic041@gmail.com>
2021-04-12 18:05:55 +0900

    Use canvas to create Image

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,142 @@
-import template from './Resources/Images/template.png';
+import template from './Resources/Images/template.jpg';
 import placeholder from './Resources/Images/placeholder.png';
 
 type itemState = {
   name: string;
   description: string;
+  image: string;
   setName: (name: string) => void;
   setDescription: (description: string) => void;
   setImage: (image: any) => void;
-  handleTextChanged: (e: any) => string;
-  handleFileChanged: (e: any) => Promise<string>;
 };
 
 const itemStore: itemState = {
-  name: '프란의 반지',
-  description: `의적으로 명성을 날린 프란의 반지
-  장비 중량이 적을수록 공격력이 높아진다
-              
-  바람의 힘을 빌어 싸웠다고 하는 프란은
-  미력하고 가난한 자들의 영웅이다
-  그리고 그들은 이것이 지어낸 이야기라고 알고 있다
-  의적으로 명성을 날린 프란의 반지
-  장비 중량이 적을수록 공격력이 높아진다
-              
-  바람의 힘을 빌어 싸웠다고 하는 프란은
-  미력하고 가난한 자들의 영웅이다
-  그리고 그들은 이것이 지어낸 이야기라고 알고 있다`,
+  name: '',
+  description: '',
+  image: '',
   setName: (name: string) => {
-    document.getElementById('itemName')!.innerText = name;
+    itemStore.name = name;
   },
   setDescription: (description: string) => {
-    document.getElementById('itemDescription')!.innerText = description;
+    itemStore.description = description;
   },
   setImage: (image: string) => {
-    let itemImage = document.getElementById('itemImage') as HTMLImageElement;
-    itemImage.src = image;
+    itemStore.image = image;
   },
-  handleTextChanged: (e) => {
-    return e.target.value;
-  },
-  handleFileChanged: (e) => {
-    return new Promise((resolve, reject) => {
-      if (e.target.files != null) {
-        let reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-        reader.onload = (e) => {
-          if (reader.result != null) {
-            let result = e.target!.result as string;
-            resolve(result);
-          }
-        };
-        reader.onerror = (e) => {
-          reject(e);
-        };
-      }
-    });
-  },
+};
+
+const getInputValue = (id: string) => {
+  return (document.getElementById(id) as HTMLInputElement).value;
+};
+
+const toBase64 = async (file: File) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+const setItem = async () => {
+  let name = getInputValue('InpItemName');
+  let description = getInputValue('InpItemDesc');
+  itemStore.setName(name);
+  itemStore.setDescription(description);
+
+  const image = document.getElementById('InpItemImage') as HTMLInputElement;
+  if (image.files) {
+    const file = image.files[0];
+    await toBase64(file)
+      .then((image) => itemStore.setImage(image))
+      .catch(() =>
+        itemStore.setImage(
+          'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+        ),
+      );
+  }
+};
+
+const createCanvas = async () => {
+  const canvas = document.getElementById('CnvImage') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  // template Image
+  const templateImage = new Image();
+  await new Promise(
+    (r) => ((templateImage.onload = r), (templateImage.src = template)),
+  );
+  ctx.drawImage(templateImage, 0, 0);
+
+  // item name
+  ctx.font = `32pt "나눔명조", "nanum myeongjo"`;
+  ctx.fillStyle = 'white';
+  ctx.fillText(itemStore.name, 50, 64);
+
+  // item description
+  ctx.font = `32pt "나눔명조", "nanum myeongjo"`;
+  const lineHeight = 58;
+  const lines = itemStore.description.split('\n');
+  for (let i = 0; i < lines.length; i++) {
+    ctx.fillText(lines[i], 50, 868 + i * lineHeight);
+  }
+
+  // item image
+  const itemImage = new Image();
+  await new Promise(
+    (r) => ((itemImage.onload = r), (itemImage.src = itemStore.image)),
+  );
+  const newSize = getNewSize(itemImage.width, itemImage.height, 350);
+  const xOffset = (canvas.width - newSize.width) / 2;
+  const yOffset = newSize.height / 2;
+
+  ctx.drawImage(
+    itemImage,
+    xOffset,
+    450 - yOffset,
+    newSize.width,
+    newSize.height,
+  );
+};
+
+const getNewSize = (width: number, height: number, target: number = 200) => {
+  const ratio = width / height;
+  let newWidth: number;
+  let newHeight: number;
+  if (ratio > 1) {
+    newWidth = target;
+    newHeight = target / ratio;
+  } else {
+    newHeight = target;
+    newWidth = target * ratio;
+  }
+  return { width: newWidth, height: newHeight };
+};
+
+const downloadImage = () => {
+  const canvas = document.getElementById('CnvImage') as HTMLCanvasElement;
+  const image = canvas.toDataURL('image/jpeg');
+  const a = document.createElement('a');
+  a.href = image;
+  const name = itemStore.name == '' ? 'untitled' : itemStore.name;
+  a.download = `${name}.jpeg`;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 };
 
 const bindItemEvents = () => {
-  document.getElementById('itemNameInput')!.addEventListener('input', (e) => {
-    itemStore.setName(itemStore.handleTextChanged(e));
-  });
   document
-    .getElementById('itemDescriptionInput')!
-    .addEventListener('input', (e) => {
-      itemStore.setDescription(itemStore.handleTextChanged(e));
+    .getElementById('BtnDownloadImage')!
+    .addEventListener('click', async () => {
+      await setItem();
+      await createCanvas();
+      downloadImage();
     });
-  document.getElementById('itemImageInput')!.addEventListener('change', (e) => {
-    itemStore
-      .handleFileChanged(e)
-      .then((image) => {
-        console.log(image);
-        itemStore.setImage(image);
-      })
-      .catch((image) => {
-        console.log(image);
-        itemStore.setImage(image);
-      });
-  });
+  document.getElementById('InpItemImage')!.addEventListener('change', () => {});
 };
 
 const init = () => {
-  let ImgTemplate = document.getElementById(
-    'templateImage',
-  )! as HTMLImageElement;
-  ImgTemplate.src = template;
-
-  let ImgPlaceholder = document.getElementById(
-    'itemImage',
-  )! as HTMLImageElement;
-  ImgPlaceholder.src = placeholder;
-
   bindItemEvents();
 };
 
